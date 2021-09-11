eacd425f4454174982816e6c085e3f8173f196d9
Uchenna Okafor <22923123+UchennaOkafor@users.noreply.github.com>
2021-09-11 23:16:11 +0100

    * Cleaned up code for and added test comment ;)

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-var yourName = prompt("Enter Your Name: "); {
+var yourName = prompt("Enter Your Name: ");
+console.log("Your Name is " + yourName);
 
-    console.log("Your Name is " + yourName);
-    
-    }
+var yourAge = prompt("Enter Your Age: ");
+
+//Another benefit to Git is that we can collaborate on the same work now!.
+//This is how it will be when we work together on Bugshare.
