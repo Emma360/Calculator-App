27f8c419559c48a6d88533520944f7673e2157a7
Anozie Emmanuel Chukwuebuka <50842909+Emma360@users.noreply.github.com>
2021-09-12 00:08:25 +0100

    Edited filename

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ console.log("Your Name is " + yourName);
 var yourAge = prompt("Enter Your Age: ");
 
 
+
 //Another benefit to Git is that we can collaborate on the same work now!.
 //This is how it will be when we work together on Bugshare.
 
