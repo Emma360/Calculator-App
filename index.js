3b49254eb36bb2d13fe1e70b9d423d15700f802f
Anozie Emmanuel Chukwuebuka <50842909+Emma360@users.noreply.github.com>
2021-09-13 23:34:13 +0100

    excess whitespaces removed

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 const number1 = parseFloat(prompt("Enter First Number: "));
-
 const operator = prompt("Enter Operator");
-
 const number2 = parseFloat(prompt("Enter Second Number: "));
-
-
 let result;
 
 if (operator == '+') {
@@ -13,12 +9,10 @@ if (operator == '+') {
     result = number1 - number2;
 } else if (operator == '*') {
     result = number1 * number2;
-
 } else if (operator == '/') {
     result = number1 / number2;
 } else {
     window.alert("Invalid Operator!")
 }
-
 
 window.alert("Result is " + result);
