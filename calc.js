8a6bd3fd10a454d1480a3e30a605d423d5abbb72
Anozie Emmanuel Chukwuebuka <50842909+Emma360@users.noreply.github.com>
2021-09-14 01:23:29 +0100

    created a new file for calculator with  functions

---
+++ calc.js
@@ -0,0 +1,34 @@
+const num1 = parseFloat(prompt("Enter number1: "));
+const operator = prompt("Enter operator: ");
+const num2 = parseFloat(prompt("Enter number2: "));
+
+
+
+function add() {
+    if (operator == '+') {
+         window.alert(num1 + num2);
+    } 
+}
+add();
+
+function subtract() {
+    if (operator == '-') {
+         window.alert(num1 - num2);
+    }
+}
+subtract();
+
+function multiply() {
+    if (operator == '*') {
+         window.alert(num1 * num2);
+    }
+}
+multiply();
+
+function divide() {
+    if (operator == '/') {
+         window.alert(num1 / num2);
+    } 
+}
+divide();
+
