dae378cc8963c6b1237b98d406a57b66c14eb1ef
Anozie Emmanuel Chukwuebuka <50842909+Emma360@users.noreply.github.com>
2021-09-19 23:39:26 +0100

    changed the documnet.write function to console.log

--- random.js
+++ random.js
@@ -1,11 +1,12 @@
 // this is to generate the first random 100 numbers
+
 let randomNumbers = Array.from({length: 100}, () => Math.floor(Math.random() * 100));
 
-document.write('This is the first random nunmbers ' + randomNumbers);
+console.log(' This is the first random numbers ' + randomNumbers );
 
 // Now the problem here is that I don't know if the genrated numbers here were from the first random numbers
 // Help me !
-let newRandomNumbers = Array.from({randomNumber, length: 10}, () => Math.floor(Math.random() * 10));
+let newRandomNumbers = Array.from({randomNumbers, length: 10}, () => Math.floor(Math.random() * 10));
 
-document.write('This is the second random numbers ' + newRandomNumbers);
+console.log(' This is the second random numbers ' + newRandomNumbers);
 
