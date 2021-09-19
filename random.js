1b3c202df4436954348c59140cdfac7462ddc842
Anozie Emmanuel Chukwuebuka <50842909+Emma360@users.noreply.github.com>
2021-09-19 23:23:10 +0100

    Changes on the variable names

--- random.js
+++ random.js
@@ -1,10 +1,11 @@
 // this is to generate the first random 100 numbers
 let randomNumbers = Array.from({length: 100}, () => Math.floor(Math.random() * 100));
 
-document.write('This is the first random nunmbers ' + randomNumber);
+document.write('This is the first random nunmbers ' + randomNumbers);
 
 // Now the problem here is that I don't know if the genrated numbers here were from the first random numbers
 // Help me !
 let newRandomNumbers = Array.from({randomNumber, length: 10}, () => Math.floor(Math.random() * 10));
 
-document.write('This is the second random numbers ' + newRnd);
+document.write('This is the second random numbers ' + newRandomNumbers);
+
